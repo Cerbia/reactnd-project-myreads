fa74ab03313dadd0d2253ee20369fa69492ac56e
Bernadetta Wąsowska <bernadetta.wasowska@deliveryhero.at>
2020-12-03 08:27:14 +0100

    Display books from store

--- src/App.js
+++ src/App.js
@@ -2,25 +2,57 @@ import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import BookList from "./BookList";
 import SearchPage from "./SearchPage";
-// import * as BooksAPI from './BooksAPI'
+
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+
+    state = {
+        reading: [{
+            title: 'A',
+            authors: 'A, B C',
+            height: 193,
+            cover: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
+        }, {
+            title: 'Ender\'s Game',
+            authors: 'Orson Scott Card',
+            height: 188,
+            cover: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")'
+            }],
+        wantTo: [{
+            title: 'Harry Potter',
+            authors: 'J. K. Rowling',
+            height: 193,
+            cover: 'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")',
+        }, {
+            title: 'Ender\'s Game',
+            authors: 'Orson Scott Card',
+            height: 188,
+            cover: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")'
+        }],
+        read: [{
+            title: 'The Adventures of Tom Sawyer',
+            authors: 'Mark Twain',
+            height: 193,
+            cover: 'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")',
+        }, {
+            title: 'Ender\'s Game',
+            authors: 'Orson Scott Card',
+            height: 188,
+            cover: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")'
+        },{
+            title: 'Ender\'s Game',
+            authors: 'Orson Scott Card',
+            height: 188,
+            cover: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")'
+        }]
+    }
 
   render() {
     return (
       <div className="app">
         <Route exact path='/' render={() => (
-            <BookList/>
+            <BookList reading={this.state.reading} wantTo={this.state.wantTo} read={this.state.read}/>
         )} />
         <Route path='/search' render={(history) => (
             <SearchPage/>
